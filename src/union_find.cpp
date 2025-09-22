5a964a0ca5037ba5a8924a3d8f203c251473bb5b
Tarik Rosin <taro00001@uni-saarland.de>
2025-09-22 12:44:31 +0200

    updated union find

--- src/union_find.cpp
+++ src/union_find.cpp
@@ -1,42 +1,43 @@
+/// Implementation of the union-find data structure
+/// It does perform path splitting but not union-by-{rank,size}
+/// when unifying two sets it chooses the one with the smaller id.
+
 #include <algorithm>
 #include "union_find.h"
 
 UnionFind::UnionFind() {}
 
 id_t UnionFind::make_set() {
-    vec.push_back(-1);
-    return static_cast<id_t>(vec.size() - 1);
+    id_t x = static_cast<id_t>(vec.size());
+    vec.push_back(x);
+    return x;
 }
 
-id_t UnionFind::find_root(id_t x) {
-    while (vec[x] >= 0) {
-        x = static_cast<id_t>(vec[x]);
+id_t UnionFind::_find_root_ph(id_t x) {
+    while (vec[x] != x) {
+        vec[x] = vec[vec[x]];
+        x = vec[x];
     }
+
     return x;
 }
 
 id_t UnionFind::unify(id_t a, id_t b) {
-    id_t ra = find_root(a);
-    id_t rb = find_root(b);
+    id_t root_a = find_root(a);
+    id_t root_b = find_root(b);
 
-    if (ra == rb) {
-        return rb;
-    }
+    if (root_a == root_b)
+        return root_a;
 
-    std::int32_t sa = -vec[ra];
-    std::int32_t sb = -vec[rb];
+    if (root_a > root_b)
+        std::swap(root_a, root_b);
 
-    if (sa > sb) {
-        std::swap(ra, rb);
-        std::swap(sa, sb);
-    }
+    vec[root_b] = root_a;
 
-    vec[rb] -= sa;
-    vec[ra] = static_cast<std::int32_t>(rb);
-
-    return rb;
+    return root_a;
 }
 
-bool UnionFind::same(id_t a, id_t b) {
-    return find_root(a) == find_root(b);
+void UnionFind::normalize() {
+    for (size_t i = 0; i < vec.size(); ++i)
+        vec[i] = vec[vec[i]];
 }
--- src/union_find.h
+++ src/union_find.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <cstdint>
 #include <vector>
 #include "id.h"
 
 class UnionFind {
 private:
-    std::vector<std::int32_t> vec;
+    std::vector<id_t> vec;
 
+    id_t _find_root_ph(id_t x);
 public:
     UnionFind();
     ~UnionFind() = default;
@@ -19,9 +19,23 @@ public:
     UnionFind& operator=(UnionFind&&) = default;
 
     id_t make_set();
-    id_t find_root(id_t x);
     id_t unify(id_t a, id_t b);
-    bool same(id_t a, id_t b);
+
+    inline id_t find_root(id_t x) {
+        // quick check which helps the branch predictor
+        // since most of our data is already canonical
+        if (vec[x] == x)
+            return x;
+
+        // iterative version with path halving
+        return _find_root_ph(x);
+    }
+
+    inline bool same(id_t a, id_t b) {
+        return find_root(a) == find_root(b);
+    }
 
     std::size_t size() const { return vec.size(); }
+
+    void normalize();
 };
