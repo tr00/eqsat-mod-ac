8d196f6d9c9d232cca95d0b95e13e03b15e01652
Tarik Rosin <tarik.rosin@mailbox.org>
2025-10-28 10:44:48 +0100

    added singleton set

---
+++ src/sets/singleton_set.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstddef>
+
+#include "../id.h"
+
+class SingletonSet
+{
+  private:
+    id_t value;
+
+  public:
+    explicit SingletonSet(id_t id)
+        : value(id)
+    {
+    }
+
+    bool contains(id_t id) const
+    {
+        return value == id;
+    }
+
+    size_t size() const
+    {
+        return 1;
+    }
+
+    bool empty() const
+    {
+        return false;
+    }
+
+    id_t get() const
+    {
+        return value;
+    }
+
+    template <typename Func>
+    void for_each(Func f) const
+    {
+        f(value);
+    }
+};
